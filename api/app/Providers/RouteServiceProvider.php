7bcd7910cfb8b35057723b49cf4c56c4dfccd8d4
falaqin <falaqin.work@gmail.com>
2023-10-31 18:52:24 +0800

    Routes for admins

--- api/app/Providers/RouteServiceProvider.php
+++ api/app/Providers/RouteServiceProvider.php
@@ -33,6 +33,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
+            Route::middleware('api')
+                ->prefix('admin/api')
+                ->group(base_path('routes/admin.php'));
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });

2670831e5ad4209b2798931ec76640aa908e58e7
falaqin <falaqin.work@gmail.com>
2023-11-03 00:09:20 +0800

    edit auth service to have slash prefix to prevent url concatenation

--- www/src/services/AuthService.js
+++ www/src/services/AuthService.js
@@ -2,12 +2,13 @@ import axios from 'axios';
 
 export default class AuthService {
     login(username, password) {
-        return axios.post('api/login', { username, password })
+        return axios.post('/api/login', { username, password })
     }
 
     logout() {
-        return axios.get('api/logout', {
+        return axios.get('/api/logout', {
             headers: {
+                "Content-Type": "application/json",
                 'Authorization': `Bearer ${localStorage.getItem('token')}`
             }
         })

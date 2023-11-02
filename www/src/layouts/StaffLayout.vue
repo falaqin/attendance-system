680284ae292e4f4ae348f907a9b6ac674dfd9076
falaqin <falaqin.work@gmail.com>
2023-11-03 00:08:52 +0800

    staff layout update, guest layout remove code

--- www/src/layouts/StaffLayout.vue
+++ www/src/layouts/StaffLayout.vue
@@ -1,3 +1,10 @@
+<script setup>
+import StaffNavbar from '../components/StaffNavbar.vue';
+
+</script>
 <template>
-    <RouterView />
+    <StaffNavbar/>
+    <div class="px-2 sm:px-56 py-2 sm:py-8">
+        <RouterView />
+    </div>
 </template>

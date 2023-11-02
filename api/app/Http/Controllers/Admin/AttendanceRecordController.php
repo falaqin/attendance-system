a83b15ebd86744319aa6562cec346e78e8eec01c
falaqin <falaqin.work@gmail.com>
2023-11-02 16:38:26 +0800

    attendance record controller for admin

---
+++ api/app/Http/Controllers/Admin/AttendanceRecordController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\AttendanceReport;
+use Illuminate\Http\Request;
+
+class AttendanceRecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $record = AttendanceReport::with('user')->get();
+
+        return response()->json([
+            'message' => 'success',
+            'record' => $record
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id, AttendanceReport $attendanceReport)
+    {
+        return response()->json([
+            'message' => 'success',
+            'record' => $attendanceReport->find($id)->load('user')
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update($id, Request $request, AttendanceReport $attendanceReport)
+    {
+        $attendanceReport = $attendanceReport->find($id)->update($request->only([
+            'status',
+            'time_clocked_in',
+            'time_clocked_out'
+        ]));
+
+        return response()->json([
+            'message' => 'success',
+            'record' => $attendanceReport->load('user')
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id, AttendanceReport $attendanceReport)
+    {
+        $attendanceReport = $attendanceReport->find($id)->delete();
+
+        return response()->json([
+            'message' => 'success',
+            'record' => $attendanceReport->load('user')
+        ]);
+    }
+}

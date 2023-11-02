9feeabe402f390cca0d1c0d3068c66296d4575d4
falaqin <falaqin.work@gmail.com>
2023-11-02 16:02:08 +0800

    create attendance report for migration

---
+++ api/database/migrations/2023_11_01_144032_create_attendance_report.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendance_report', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->string('status');
+            $table->timestamp('time_clocked_in');
+            $table->timestamp('time_clocked_out')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendance_report');
+    }
+};
